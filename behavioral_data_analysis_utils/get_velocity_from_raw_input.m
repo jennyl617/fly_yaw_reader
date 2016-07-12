b00b0f836619593dae9b862d88d02938b06f7423
Sasha Rayshubskiy <druzhiche@gmail.com>
2016-07-12 07:58:39 -0400

    none

--- behavioral_data_analysis_utils/get_velocity_from_raw_input.m
+++ behavioral_data_analysis_utils/get_velocity_from_raw_input.m
@@ -15,11 +15,13 @@ time_downsampled = squeeze(mean(reshape(time(1:cut_length), [dt, x])));
 
 %%%%% 
 % Turn these on to recalibrate the average and std of zero volts.
-%figure;
-%hold on;
-%plot(smoothedData_downsampled);
-%plot([1:length(smoothedData_downsampled)], repmat(mean(smoothedData_downsampled), [length(smoothedData_downsampled)] ));
-%disp(['mean: ' num2str(mean(smoothedData_downsampled)) ' std: ' num2str(2.0*std(smoothedData_downsampled,1))]);
+if 0
+figure;
+hold on;
+plot(smoothedData_downsampled);
+plot([1:length(smoothedData_downsampled)], repmat(mean(smoothedData_downsampled), [length(smoothedData_downsampled)] ));
+disp(['mean: ' num2str(mean(smoothedData_downsampled)) ' std: ' num2str(2.0*std(smoothedData_downsampled,1))]);
+end
 
 sdz = smoothedData_downsampled - repmat(zero_volt_mark, [1 size(smoothedData_downsampled,2)]);
 
--- behavioral_data_analysis_utils/sensor_settings.m
+++ behavioral_data_analysis_utils/sensor_settings.m
@@ -9,9 +9,9 @@ settings.stim = 0.5;
 settings.post_stim = 3.0;
 
 % Processing settings
-settings.zero_mean_voltage_per_channel = [2.477, 2.463, 2.488, 2.484];
+settings.zero_mean_voltage_per_channel = [2.4682, 2.4541, 2.4725, 2.4685];
 %settings.zero_mean_two_std_per_channel = [0.0137, 0.0151, 0.0142, 0.0156];
-settings.zero_mean_two_std_per_channel = [0.00097875, 0.0010688, 0.0011572, 0.0020838];
+settings.zero_mean_two_std_per_channel = [0.001, 0.001, 0.001, 0.001];
 settings.cutoffFreq = 50;
 settings.aiType = 'SingleEnded';
 settings.sensorPollFreq = 100; 
